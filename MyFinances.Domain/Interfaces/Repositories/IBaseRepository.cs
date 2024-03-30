404978586d176856059c437a7767434f6d9c5fc5
Pikatoise <wierwolf@inbox.ru>
2024-03-30 16:48:35 +0500

    Init & define IBaseRepository

---
+++ MyFinances.Domain/Interfaces/Repositories/IBaseRepository.cs
@@ -0,0 +1,13 @@
+﻿namespace MyFinances.Domain.Interfaces.Repositories
+{
+    public interface IBaseRepository<TEntity>
+    {
+        IQueryable<TEntity> GetAll();
+
+        Task<TEntity> CreateAsync(TEntity entity);
+
+        TEntity Update(TEntity entity);
+
+        TEntity Delete(TEntity entity);
+    }
+}
