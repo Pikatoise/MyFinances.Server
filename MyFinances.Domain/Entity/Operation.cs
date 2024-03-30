84a3b97a7df1f278c3f6d5cbf3adc65e55dcef2e
Pikatoise <wierwolf@inbox.ru>
2024-03-30 15:20:23 +0500

    Define operation entity

--- MyFinances.Domain/Entity/Operation.cs
+++ MyFinances.Domain/Entity/Operation.cs
@@ -1,9 +1,20 @@
-﻿namespace MyFinances.Domain.Entity
+﻿using MyFinances.Domain.Interfaces.Validations;
+
+namespace MyFinances.Domain.Entity
 {
     /// <summary>
     /// Finance operation of user in certain period
     /// </summary>
-    public class Operation
+    public class Operation: IEntityId<int>
     {
+        public int Id { get; set; }
+
+        public int PeriodId { get; set; }
+
+        public Period Period { get; set; } = null!;
+
+        public required string Title { get; set; }
+
+        public double Amount { get; set; }
     }
 }
