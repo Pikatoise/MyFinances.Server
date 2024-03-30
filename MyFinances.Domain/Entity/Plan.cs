51e27fda35923b46f729bcdeeca85cea15f8f9f2
Pikatoise <wierwolf@inbox.ru>
2024-03-30 15:58:11 +0500

    Define plan entity

--- MyFinances.Domain/Entity/Plan.cs
+++ MyFinances.Domain/Entity/Plan.cs
@@ -1,9 +1,26 @@
-﻿namespace MyFinances.Domain.Entity
+﻿using MyFinances.Domain.Interfaces.Validations;
+
+namespace MyFinances.Domain.Entity
 {
     /// <summary>
     /// A plan to make some financial operation in the future
     /// </summary>
-    public class Plan
+    public class Plan: IEntityId<int>, IAuditable
     {
+        public int Id { get; set; }
+
+        public required string Name { get; set; }
+
+        public DateTime FinalDate { get; set; }
+
+        public bool Status { get; set; }
+
+        public int TypeId { get; set; }
+
+        public OperationType Type { get; set; } = null!;
+
+        public DateTime CreatedAt { get; set; }
+
+        public DateTime? UpdatedAt { get; set; }
     }
 }
