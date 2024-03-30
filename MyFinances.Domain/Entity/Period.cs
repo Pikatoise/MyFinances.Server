564a1630fbd091b71ad1d3d9aa13dc58e5336110
Pikatoise <wierwolf@inbox.ru>
2024-03-30 15:13:11 +0500

    Define period entity

--- MyFinances.Domain/Entity/Period.cs
+++ MyFinances.Domain/Entity/Period.cs
@@ -1,9 +1,22 @@
-﻿namespace MyFinances.Domain.Entity
+﻿using MyFinances.Domain.Interfaces.Validations;
+
+namespace MyFinances.Domain.Entity
 {
     /// <summary>
     /// Period with operations in current month:year
     /// </summary>
-    public class Period
+    public class Period: IEntityId<int>
     {
+        public int Id { get; set; }
+
+        public int UserId { get; set; }
+
+        public User User { get; set; } = null!;
+
+        public List<Operation> Operations { get; set; } = [];
+
+        public int Month { get; set; }
+
+        public int Year { get; set; }
     }
 }
