61c253ce4a1254a9262f186a511e70814d898ff2
Pikatoise <wierwolf@inbox.ru>
2024-03-30 16:52:30 +0500

    Init & define IUnitOfWork

---
+++ MyFinances.Domain/Interfaces/Repositories/IUnitOfWork.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.EntityFrameworkCore.Storage;
+using MyFinances.Domain.Entity;
+
+namespace MyFinances.Domain.Interfaces.Repositories
+{
+    public interface IUnitOfWork
+    {
+        Task<IDbContextTransaction> BeginTransactionAsync();
+
+        Task<int> SaveChangesAsync();
+
+        IBaseRepository<User> Users { get; }
+
+        IBaseRepository<UserRole> UserRoles { get; }
+
+        IBaseRepository<UserToken> UserTokens { get; }
+
+        IBaseRepository<Role> Roles { get; }
+
+        IBaseRepository<TypeAssociation> TypeAssociations { get; }
+
+        IBaseRepository<OperationType> OperationTypes { get; }
+
+        IBaseRepository<Plan> Plans { get; }
+
+        IBaseRepository<Period> Periods { get; }
+
+        IBaseRepository<Operation> Operations { get; }
+    }
+}
