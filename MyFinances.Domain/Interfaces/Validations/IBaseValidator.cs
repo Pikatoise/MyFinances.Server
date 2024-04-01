115488812c0ea65f5d5fc0f73feed661618dacf2
Pikatoise <wierwolf@inbox.ru>
2024-04-01 16:19:12 +0500

    Init & define IBaseValidator

---
+++ MyFinances.Domain/Interfaces/Validations/IBaseValidator.cs
@@ -0,0 +1,9 @@
+﻿using MyFinances.Domain.Result;
+
+namespace MyFinances.Domain.Interfaces.Validations
+{
+    public interface IBaseValidator<in T> where T : class
+    {
+        BaseResult ValidateOnNull(T? model);
+    }
+}
