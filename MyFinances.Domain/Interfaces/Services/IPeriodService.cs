fbb26c4ed5fe165b595b3c1e4d61b237fcdaf16f
Pikatoise <wierwolf@inbox.ru>
2024-04-02 17:23:58 +0500

    Add ProfitOfPeriod method to period service interface

--- MyFinances.Domain/Interfaces/Services/IPeriodService.cs
+++ MyFinances.Domain/Interfaces/Services/IPeriodService.cs
@@ -1,9 +1,17 @@
-﻿namespace MyFinances.Domain.Interfaces.Services
+﻿using MyFinances.Domain.Result;
+
+namespace MyFinances.Domain.Interfaces.Services
 {
     /// <summary>
     /// Service responsible for period controle
     /// </summary>
     public interface IPeriodService
     {
+        /// <summary>
+        /// Get profit of all operations by period
+        /// </summary>
+        /// <param name="periodId">Period identificator</param>
+        /// <returns>Profit: double</returns>
+        Task<BaseResult<double>> ProfitOfPeriod(int periodId);
     }
 }
