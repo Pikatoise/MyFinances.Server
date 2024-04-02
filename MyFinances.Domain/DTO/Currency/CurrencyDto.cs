0ef9f4220d84e82a105b8dcfbddf10b1d650e2e0
Pikatoise <wierwolf@inbox.ru>
2024-04-02 16:37:12 +0500

    Init & define CurrencyDto, define currency service interface

---
+++ MyFinances.Domain/DTO/Currency/CurrencyDto.cs
@@ -0,0 +1,4 @@
+﻿namespace MyFinances.Domain.DTO.Currency
+{
+    public record CurrencyDto(string Value);
+}
--- MyFinances.Domain/Interfaces/Services/ICurrencyService.cs
+++ MyFinances.Domain/Interfaces/Services/ICurrencyService.cs
@@ -1,9 +1,29 @@
-﻿namespace MyFinances.Domain.Interfaces.Services
+﻿using MyFinances.Domain.DTO.Currency;
+using MyFinances.Domain.Result;
+
+namespace MyFinances.Domain.Interfaces.Services
 {
     /// <summary>
     /// Services responsible for currency 
     /// </summary>
     public interface ICurrencyService
     {
+        /// <summary>
+        /// Get value of requested currency
+        /// </summary>
+        /// <param name="currencyName">Abbreviation of currency</param>
+        /// <returns>
+        /// CurrencyDto:
+        /// {
+        ///     "Value": double converted to string
+        /// }
+        /// </returns>
+        Task<BaseResult<CurrencyDto>> GetCurrencyValue(string currencyName);
+
+        /// <summary>
+        /// Update server stored value of available currency
+        /// </summary>
+        /// <returns>A boolean value that indicates whether the update is successful</returns>
+        Task<BaseResult<bool>> UpdateCurrency();
     }
 }
