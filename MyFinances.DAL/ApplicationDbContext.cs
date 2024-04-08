c016c2b310f76bfc4ed49bf6d1beaa1fa439dcdb
Pikatoise <wierwolf@inbox.ru>
2024-04-08 20:43:32 +0500

    Init DAL structure & define ApplicationDbContext

---
+++ MyFinances.DAL/ApplicationDbContext.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System.Reflection;
+
+namespace MyFinances.DAL
+{
+    public class ApplicationDbContext(DbContextOptions<ApplicationDbContext> options): DbContext(options)
+    {
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            base.OnConfiguring(optionsBuilder);
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.ApplyConfigurationsFromAssembly(Assembly.GetExecutingAssembly());
+        }
+    }
+}
