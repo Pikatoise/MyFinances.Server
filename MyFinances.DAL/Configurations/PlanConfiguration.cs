842ec815f91de5f9f703ee8a2ddd15efbd149260
Pikatoise <wierwolf@inbox.ru>
2024-04-08 22:36:50 +0500

    Init & define PlanConfiguration, add relation of Plans and Operations to OperationType

---
+++ MyFinances.DAL/Configurations/PlanConfiguration.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using MyFinances.Domain.Entity;
+
+namespace MyFinances.DAL.Configurations
+{
+    public class PlanConfiguration: IEntityTypeConfiguration<Plan>
+    {
+        public void Configure(EntityTypeBuilder<Plan> builder)
+        {
+            builder.Property(x => x.Id).ValueGeneratedOnAdd();
+            builder.Property(x => x.FinalDate).IsRequired();
+            builder.Property(x => x.Name).IsRequired().HasMaxLength(50);
+            builder.Property(x => x.Status).IsRequired();
+
+            builder.HasOne<User>(x => x.User)
+                .WithMany(x => x.Plans)
+                .HasForeignKey(x => x.UserId);
+
+            builder.HasOne<OperationType>(x => x.Type)
+                .WithMany(x => x.Plans)
+                .HasForeignKey(x => x.TypeId);
+        }
+    }
+}
--- MyFinances.Domain/Entity/OperationType.cs
+++ MyFinances.Domain/Entity/OperationType.cs
@@ -11,6 +11,10 @@ namespace MyFinances.Domain.Entity
 
         public required string IconSrc { get; set; }
 
+        public List<Operation> Operations { get; set; } = [];
+
+        public List<Plan> Plans { get; set; } = [];
+
         public List<TypeAssociation> Associations { get; set; } = [];
 
         public DateTime CreatedAt { get; set; }
