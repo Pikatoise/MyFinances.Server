f6b36db7cad5cf60b229310573a8bb61c34beb63
Pikatoise <wierwolf@inbox.ru>
2024-04-08 21:56:20 +0500

    Init & define DI of DAL, add empty pg conn to appsettings

---
+++ MyFinances.DAL/DI/DependencyInjection.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using MyFinances.DAL.Interceptors;
+using MyFinances.DAL.Repositories;
+using MyFinances.Domain.Entity;
+using MyFinances.Domain.Interfaces.Repositories;
+
+namespace MyFinances.DAL.DI
+{
+    public static class DependencyInjection
+    {
+        public static void AddDataAccessLayer(this IServiceCollection services, IConfiguration configuration)
+        {
+            var connectionString = configuration.GetConnectionString("pgConn");
+
+            services.AddSingleton<DateInterceptor>();
+            services.AddDbContext<ApplicationDbContext>((sp, options) =>
+            {
+                options.UseNpgsql(connectionString);
+                options.AddInterceptors(sp.GetRequiredService<DateInterceptor>());
+            });
+            services.InitRepositories();
+        }
+
+        private static void InitRepositories(this IServiceCollection services)
+        {
+            services.AddScoped<IBaseRepository<User>, BaseRepository<User>>();
+            services.AddScoped<IBaseRepository<UserRole>, BaseRepository<UserRole>>();
+            services.AddScoped<IBaseRepository<UserToken>, BaseRepository<UserToken>>();
+            services.AddScoped<IBaseRepository<Role>, BaseRepository<Role>>();
+            services.AddScoped<IBaseRepository<TypeAssociation>, BaseRepository<TypeAssociation>>();
+            services.AddScoped<IBaseRepository<OperationType>, BaseRepository<OperationType>>();
+            services.AddScoped<IBaseRepository<Plan>, BaseRepository<Plan>>();
+            services.AddScoped<IBaseRepository<Period>, BaseRepository<Period>>();
+            services.AddScoped<IBaseRepository<Operation>, BaseRepository<Operation>>();
+
+            services.AddScoped<IUnitOfWork, UnitOfWork>();
+        }
+    }
+}
