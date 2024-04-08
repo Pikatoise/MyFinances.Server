8689d33702efdb7d357a12c453e35c74153cd275
Pikatoise <wierwolf@inbox.ru>
2024-04-08 22:09:26 +0500

    Init & define UserConfiguration & UserRoleConfiguration

---
+++ MyFinances.DAL/Configurations/UserConfiguration.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using MyFinances.Domain.Entity;
+
+namespace MyFinances.DAL.Configurations
+{
+    public class UserConfiguration: IEntityTypeConfiguration<User>
+    {
+        public void Configure(EntityTypeBuilder<User> builder)
+        {
+            builder.Property(x => x.Id).ValueGeneratedOnAdd();
+            builder.Property(x => x.Login).IsRequired().HasMaxLength(100);
+            builder.Property(x => x.Password).IsRequired();
+
+            builder.HasMany<Period>(x => x.Periods)
+                .WithOne(x => x.User)
+                .HasForeignKey(x => x.UserId)
+                .HasPrincipalKey(x => x.Id);
+
+            builder.HasMany<Plan>(x => x.Plans)
+                .WithOne(x => x.User)
+                .HasForeignKey(x => x.UserId)
+                .HasPrincipalKey(x => x.Id);
+
+            builder.HasMany(x => x.Roles)
+                .WithMany(x => x.Users)
+                .UsingEntity<UserRole>(
+                    x => x.HasOne<Role>().WithMany().HasForeignKey(x => x.RoleId),
+                    x => x.HasOne<User>().WithMany().HasForeignKey(x => x.UserId)
+                );
+        }
+    }
+}
---
+++ MyFinances.DAL/Configurations/UserRoleConfiguration.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using MyFinances.Domain.Entity;
+
+namespace MyFinances.DAL.Configurations
+{
+    public class UserRoleConfiguration: IEntityTypeConfiguration<UserRole>
+    {
+        public void Configure(EntityTypeBuilder<UserRole> builder)
+        {
+        }
+    }
+}
