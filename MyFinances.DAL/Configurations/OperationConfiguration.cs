7cfd863a115ce29ad2662b69c9a2f2dfe3dfc82f
Pikatoise <wierwolf@inbox.ru>
2024-04-08 22:52:59 +0500

    Init & define OperationConfiguration

---
+++ MyFinances.DAL/Configurations/OperationConfiguration.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using MyFinances.Domain.Entity;
+
+namespace MyFinances.DAL.Configurations
+{
+    public class OperationConfiguration: IEntityTypeConfiguration<Operation>
+    {
+        public void Configure(EntityTypeBuilder<Operation> builder)
+        {
+            builder.HasKey(x => x.Id);
+
+            builder.Property(x => x.Id).ValueGeneratedOnAdd();
+            builder.Property(x => x.Amount).IsRequired();
+            builder.Property(x => x.Title).IsRequired().HasMaxLength(50);
+
+            builder.HasOne<OperationType>(x => x.Type)
+                .WithMany(x => x.Operations)
+                .HasForeignKey(x => x.TypeId);
+
+            builder.HasOne<Period>(x => x.Period)
+                .WithMany(x => x.Operations)
+                .HasForeignKey(x => x.PeriodId);
+        }
+    }
+}
