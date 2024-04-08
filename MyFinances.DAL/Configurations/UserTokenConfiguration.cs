9e3c1807aaf7fd52e9c22f593c233cb550b35608
Pikatoise <wierwolf@inbox.ru>
2024-04-08 22:11:03 +0500

    Init & define UserTokenConfiguration

---
+++ MyFinances.DAL/Configurations/UserTokenConfiguration.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using MyFinances.Domain.Entity;
+
+namespace MyFinances.DAL.Configurations
+{
+    public class UserTokenConfiguration: IEntityTypeConfiguration<UserToken>
+    {
+        public void Configure(EntityTypeBuilder<UserToken> builder)
+        {
+            builder.Property(x => x.Id).ValueGeneratedOnAdd();
+            builder.Property(x => x.RefreshToken).IsRequired();
+            builder.Property(x => x.RefreshTokenExpiryTime).IsRequired();
+        }
+    }
+}
