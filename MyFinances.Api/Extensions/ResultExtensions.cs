17756489ceb991b0fcc1cd3d7b7c904169ca702c
Pikatoise <wierwolf@inbox.ru>
2024-04-10 18:22:54 +0500

    Init & define ResultExtension

---
+++ MyFinances.Api/Extensions/ResultExtensions.cs
@@ -0,0 +1,27 @@
+﻿using MyFinances.Domain.Result;
+
+namespace MyFinances.Api.Extensions
+{
+    public static class ResultExtensions
+    {
+        /// <summary>
+        /// Convert result to HTTP response
+        /// </summary>
+        /// <param name="result"></param>
+        /// <returns></returns>
+        /// <exception cref="InvalidOperationException"></exception>
+        public static IResult ToProblemDetails(this BaseResult result)
+        {
+            if (result.IsSuccess)
+                throw new InvalidOperationException();
+
+            return Results.Problem(
+                statusCode: result.ErrorCode,
+                title: "Bad request",
+                extensions: new Dictionary<string, object?>
+                {
+                    { "errors", result.ErrorMessage }
+                });
+        }
+    }
+}
