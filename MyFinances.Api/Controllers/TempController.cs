797b4a6698b266da18c48728e7ab0b2622c53e20
Pikatoise <wierwolf@inbox.ru>
2024-04-10 19:37:19 +0500

    Finally define ToProblemDetails in ResultExtensions & test work of Result

--- MyFinances.Api/Controllers/TempController.cs
+++ MyFinances.Api/Controllers/TempController.cs
@@ -1,5 +1,7 @@
 using Asp.Versioning;
 using Microsoft.AspNetCore.Mvc;
+using MyFinances.Api.Extensions;
+using MyFinances.Domain.Result;
 
 namespace MyFinances.Api.Controllers
 {
@@ -16,21 +18,28 @@ namespace MyFinances.Api.Controllers
         /// <summary>
         /// Empty get request
         /// </summary>
-        /// <param name="message">Test message (can be empty)</param>
         /// <remarks>
         /// Request for test get:
         /// 
-        ///     GET(message)
+        ///     GET()
         ///     
         /// </remarks>
         /// <response code="200">Ok - request successfuly worked out</response>
         /// <response code="400">Fail - server can't process request</response>
-        [HttpGet("{message}")]
-        public ActionResult<string> Get(string message = "NONE")
+        [HttpGet()]
+        public IResult Get()
         {
             _logger.LogDebug($"Get request --- {DateTime.Now.ToShortTimeString()}");
 
-            return Ok($"Success\nMessage: {message}");
+            var result = new BaseResult<string>()
+            {
+                Failure = Error.Conflict("User.Conflit", "Some conflict")
+            };
+
+            if (result.IsSuccess)
+                return Results.Ok("Successful request");
+            else
+                return result.ToProblemDetails();
         }
     }
 }
--- MyFinances.Api/Extensions/ResultExtensions.cs
+++ MyFinances.Api/Extensions/ResultExtensions.cs
@@ -1,7 +1,11 @@
-﻿using MyFinances.Domain.Result;
+﻿using MyFinances.Domain.Enum;
+using MyFinances.Domain.Result;
 
 namespace MyFinances.Api.Extensions
 {
+    /// <summary>
+    /// Extensions for Result entity
+    /// </summary>
     public static class ResultExtensions
     {
         /// <summary>
@@ -12,16 +16,38 @@ namespace MyFinances.Api.Extensions
         /// <exception cref="InvalidOperationException"></exception>
         public static IResult ToProblemDetails(this BaseResult result)
         {
-            if (result.IsSuccess)
+            if (result.IsSuccess || result.Failure == null)
                 throw new InvalidOperationException();
 
-            return Results.Problem(
-                statusCode: result.ErrorCode,
-                title: "Bad request",
-                extensions: new Dictionary<string, object?>
-                {
-                    { "errors", result.ErrorMessage }
-                });
+            return Results.Problem
+                (
+                    statusCode: GetStatusCode(result.Failure.Type),
+                    title: GetTitle(result.Failure.Type),
+                    extensions: new Dictionary<string, object?>
+                    {
+                        { "errors", result.Failure }
+                    }
+                );
+
         }
+
+        static int GetStatusCode(ErrorType errorType) =>
+            errorType switch
+            {
+                ErrorType.Validation => StatusCodes.Status400BadRequest,
+                ErrorType.NotFound => StatusCodes.Status404NotFound,
+                ErrorType.Conflict => StatusCodes.Status409Conflict,
+                _ => StatusCodes.Status500InternalServerError
+            };
+
+        static string GetTitle(ErrorType errorType) =>
+            errorType switch
+            {
+                ErrorType.Failure => "Server Failure",
+                ErrorType.Validation => "Incorrect data",
+                ErrorType.NotFound => "No data found",
+                ErrorType.Conflict => "Caused conflict",
+                _ => "Unexpected failure"
+            };
     }
 }
