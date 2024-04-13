dabfb917bdb22f675305a4ffee6737de9d35612d
Pikatoise <wierwolf@inbox.ru>
2024-04-13 17:44:03 +0500

    Add comments to RequestLogContextMiddleware

--- MyFinances.Api/Middlewares/RequestLogContextMiddleware.cs
+++ MyFinances.Api/Middlewares/RequestLogContextMiddleware.cs
@@ -3,13 +3,18 @@
 namespace MyFinances.Api.Middlewares
 {
     /// <summary>
-    /// 
+    /// Handle received request for logging
     /// </summary>
     /// <param name="next">Next middleware</param>
     public class RequestLogContextMiddleware(RequestDelegate next)
     {
         private readonly RequestDelegate _next = next;
 
+        /// <summary>
+        /// Add identificator for request
+        /// </summary>
+        /// <param name="context"></param>
+        /// <returns></returns>
         public Task InvokeAsync(HttpContext context)
         {
             using (LogContext.PushProperty("CorrelationId", context.TraceIdentifier))
