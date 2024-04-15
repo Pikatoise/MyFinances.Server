143f4575a7daf56d0c8ac9bb1359b80dd15dc8c7
Pikatoise <wierwolf@inbox.ru>
2024-04-15 19:28:37 +0500

    Init ICurrencyValidator, add comments to IBaseValidator

--- MyFinances.Domain/Interfaces/Validations/IBaseValidator.cs
+++ MyFinances.Domain/Interfaces/Validations/IBaseValidator.cs
@@ -2,8 +2,17 @@
 
 namespace MyFinances.Domain.Interfaces.Validations
 {
+    /// <summary>
+    /// Base interface with common validators
+    /// </summary>
+    /// <typeparam name="T">Domain entity, for which the validator is</typeparam>
     public interface IBaseValidator<in T> where T : class
     {
+        /// <summary>
+        /// Check the <typeparamref name="T"/> is empty
+        /// </summary>
+        /// <param name="model">Entity</param>
+        /// <returns></returns>
         BaseResult ValidateOnNull(T? model);
     }
 }
