6c77ac5481915d8b79ad9649559885a49a4efc04
Pikatoise <wierwolf@inbox.ru>
2024-04-17 18:37:18 +0500

    Init & define CurrencyValidator, add ErrorMessages resource file

---
+++ MyFinances.Application/Validations/ServiceValidations/CurrencyValidator.cs
@@ -0,0 +1,21 @@
+﻿using MyFinances.Application.Resources;
+using MyFinances.Domain.Entity;
+using MyFinances.Domain.Interfaces.Validations;
+using MyFinances.Domain.Result;
+
+namespace MyFinances.Application.Validations.ServiceValidations
+{
+    public class CurrencyValidator: ICurrencyValidator
+    {
+        public BaseResult ValidateOnNull(Currency? model)
+        {
+            if (model == null)
+                return new BaseResult()
+                {
+                    Failure = Error.Validation("Currency.NotExist", ErrorMessages.Currency_NotExist)
+                };
+
+            return new BaseResult();
+        }
+    }
+}
