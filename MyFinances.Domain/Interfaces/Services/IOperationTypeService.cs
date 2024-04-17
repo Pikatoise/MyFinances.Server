d8c24d1040aff94de5e8a9bb8bdb3cb9d39dcbc4
Pikatoise <wierwolf@inbox.ru>
2024-04-17 17:40:28 +0500

    Add AddTypeAssociation method to IOperationTypeService

--- MyFinances.Domain/Interfaces/Services/IOperationTypeService.cs
+++ MyFinances.Domain/Interfaces/Services/IOperationTypeService.cs
@@ -13,12 +13,20 @@ namespace MyFinances.Domain.Interfaces.Services
         /// </summary>
         /// <param name="association">User input association</param>
         /// <returns><c>Collection of OperationTypeDto</c>: type with id and image source</returns>
-        Task<CollectionResult<OperationTypeDto>> GetTypeByAssociation(string association);
+        Task<CollectionResult<OperationTypeDto>> GetTypesByAssociation(string association);
 
         /// <summary>
         /// Get all available types of operation
         /// </summary>
         /// <returns><c>Collection of OperationTypeDto</c>: type with id and image source</returns>
         Task<CollectionResult<OperationTypeDto>> GetAllTypes();
+
+        /// <summary>
+        /// Add new association to operation type
+        /// </summary>
+        /// <param name="typeId">Associated type</param>
+        /// <param name="association">New association</param>
+        /// <returns>Type to which the association was added</returns>
+        Task<BaseResult<OperationTypeDto>> AddTypeAssociation(int typeId, string association);
     }
 }
