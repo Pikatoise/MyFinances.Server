56d4669e2d46feae98e01871af026bb7f06ecd91
Pikatoise <wierwolf@inbox.ru>
2024-04-19 15:20:10 +0500

    Init & define IFixerService

---
+++ MyFinances.Domain/Interfaces/Services/IFixerService.cs
@@ -0,0 +1,17 @@
+﻿using MyFinances.Domain.Entity;
+using MyFinances.Domain.Result;
+
+namespace MyFinances.Domain.Interfaces.Services
+{
+    /// <summary>
+    /// Service responsible to access external currency API
+    /// </summary>
+    public interface IFixerService
+    {
+        /// <summary>
+        /// Get all supported currencies from API
+        /// </summary>
+        /// <returns><c>Collection of Currency</c>: fresh currencies from api</returns>
+        Task<CollectionResult<Currency>> GetCurrencies();
+    }
+}
