62a1d19e8e17fb0c212e070540cdcdf50f167465
Pikatoise <wierwolf@inbox.ru>
2024-04-19 19:40:03 +0500

    Init & define DI for Application

---
+++ MyFinances.Application/DI/DependencyInjection.cs
@@ -0,0 +1,41 @@
+﻿using FluentValidation;
+using Microsoft.Extensions.DependencyInjection;
+using MyFinances.Application.Mapping;
+using MyFinances.Application.Services;
+using MyFinances.Application.Validations.DtoValidations.Operation;
+using MyFinances.Application.Validations.DtoValidations.Plan;
+using MyFinances.Application.Validations.ServiceValidations;
+using MyFinances.Domain.DTO.Operation;
+using MyFinances.Domain.DTO.Plan;
+using MyFinances.Domain.Interfaces.Services;
+using MyFinances.Domain.Interfaces.Validations;
+
+namespace MyFinances.Application.DI
+{
+    public static class DependencyInjection
+    {
+        public static void AddApplication(this IServiceCollection services)
+        {
+            services.AddAutoMapper(typeof(CurrencyMapping));
+
+            InitServices(services);
+        }
+
+        private static void InitServices(this IServiceCollection services)
+        {
+            services.AddScoped<ICurrencyValidator, CurrencyValidator>();
+            services.AddScoped<IOperationTypeValidator, OperationTypeValidator>();
+            services.AddScoped<IOperationValidator, OperationValidator>();
+            services.AddScoped<IPeriodValidator, PeriodValidator>();
+            services.AddScoped<IPlanValidator, PlanValidator>();
+
+            services.AddScoped<IValidator<CreateOperationDto>, CreateOperationValidator>();
+            services.AddScoped<IValidator<UpdateOperationDto>, UpdateOperationValidator>();
+            services.AddScoped<IValidator<CreatePlanDto>, CreatePlanValidator>();
+            services.AddScoped<IValidator<CreatePlanDto>, CreatePlanValidator>();
+
+            services.AddScoped<ICurrencyService, CurrencyService>();
+            services.AddScoped<IFixerService, FixerService>();
+        }
+    }
+}
