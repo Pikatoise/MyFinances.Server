58768caed03ec7b1447971be4dba86d9160fa7ba
Pikatoise <wierwolf@inbox.ru>
2024-04-19 14:23:42 +0500

    Init & define CurrencyConfiguration

---
+++ MyFinances.DAL/Configurations/CurrencyConfiguration.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using MyFinances.Domain.Entity;
+
+namespace MyFinances.DAL.Configurations
+{
+    public class CurrencyConfiguration: IEntityTypeConfiguration<Currency>
+    {
+        public void Configure(EntityTypeBuilder<Currency> builder)
+        {
+            builder.HasKey(x => x.Name);
+
+            builder.Property(x => x.Name).IsRequired();
+            builder.Property(x => x.Value).IsRequired();
+        }
+    }
+}
