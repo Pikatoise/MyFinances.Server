5caaa5b6c1d158cac91c2efd638b6314f89860ef
Pikatoise <wierwolf@inbox.ru>
2024-04-28 21:06:36 +0500

    Init PeriodService

---
+++ MyFinances.Application/Services/PeriodService.cs
@@ -0,0 +1,39 @@
+﻿using MyFinances.Domain.DTO.Period;
+using MyFinances.Domain.Interfaces.Services;
+using MyFinances.Domain.Result;
+
+namespace MyFinances.Application.Services
+{
+    public class PeriodService: IPeriodService
+    {
+        public Task<BaseResult<PeriodDto>> CreateNewPeriod(int userId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<BaseResult<PeriodDto?>> CurrentPeriodByUserId(int userId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<BaseResult<PeriodDto?>> DeletePeriod(int periodId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<BaseResult<PeriodDto?>> GetByYearAndMonth(int userId, int year, int month)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<CollectionResult<PeriodDto>> PeriodsPaging(int userId, int currentPage, int step, string order = "asc")
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<BaseResult<double>> ProfitOfPeriod(int periodId)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
