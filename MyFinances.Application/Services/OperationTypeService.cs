88cb8938ce1f36c30bf0932612aa16f256506868
Pikatoise <wierwolf@inbox.ru>
2024-04-30 19:36:29 +0500

    Init OperationTypeService

---
+++ MyFinances.Application/Services/OperationTypeService.cs
@@ -0,0 +1,37 @@
+﻿using AutoMapper;
+using MyFinances.Domain.DTO.OperationType;
+using MyFinances.Domain.Interfaces.Repositories;
+using MyFinances.Domain.Interfaces.Services;
+using MyFinances.Domain.Interfaces.Validations;
+using MyFinances.Domain.Result;
+using Serilog;
+
+namespace MyFinances.Application.Services
+{
+    public class OperationTypeService(
+        ILogger logger,
+        IUnitOfWork unitOfWork,
+        IOperationTypeValidator operationTypeValidator,
+        IMapper mapper): IOperationTypeService
+    {
+        private readonly ILogger _logger = logger;
+        private readonly IUnitOfWork _unitOfWork = unitOfWork;
+        private readonly IOperationTypeValidator _operationTypeValidator = operationTypeValidator;
+        private readonly IMapper _mapper = mapper;
+
+        public Task<BaseResult<OperationTypeDto>> AddTypeAssociation(int typeId, string association)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<CollectionResult<OperationTypeDto>> GetAllTypes()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<CollectionResult<OperationTypeDto>> GetTypesByAssociation(string association)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
