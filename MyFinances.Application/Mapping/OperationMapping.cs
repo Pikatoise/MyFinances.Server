979c6499e12973c74de4611f16f19f1c37a897c4
Pikatoise <wierwolf@inbox.ru>
2024-04-30 21:39:38 +0500

    Change CreatedAt field of OperationDto to string, init & define OperationMapping

---
+++ MyFinances.Application/Mapping/OperationMapping.cs
@@ -0,0 +1,17 @@
+﻿using AutoMapper;
+using MyFinances.Domain.DTO.Operation;
+using MyFinances.Domain.Entity;
+
+namespace MyFinances.Application.Mapping
+{
+    public class OperationMapping: Profile
+    {
+        public OperationMapping()
+        {
+            CreateMap<Operation, OperationDto>()
+                .ForCtorParam("CreatedAt", o => o.MapFrom(k => k.CreatedAt.ToLongDateString()))
+                .ReverseMap()
+                .ForMember(dest => dest.CreatedAt, opt => opt.MapFrom(src => DateTime.Parse(src.CreatedAt)));
+        }
+    }
+}
