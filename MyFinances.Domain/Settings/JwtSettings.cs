3fa557f24012641324f138966a88150d12dfde1e
Pikatoise <wierwolf@inbox.ru>
2024-05-02 19:14:23 +0500

    Init JwtSettings

---
+++ MyFinances.Domain/Settings/JwtSettings.cs
@@ -0,0 +1,19 @@
+﻿namespace MyFinances.Domain.Settings
+{
+    public class JwtSettings
+    {
+        public const string DefaultSection = "Jwt";
+
+        public required string Issuer { get; set; }
+
+        public required string Audience { get; set; }
+
+        public required string Authority { get; set; }
+
+        public required string JwtKey { get; set; }
+
+        public int Lifetime { get; set; }
+
+        public int RefreshTokenValidityInDays { get; set; }
+    }
+}
