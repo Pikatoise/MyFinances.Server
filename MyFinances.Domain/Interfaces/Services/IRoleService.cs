2bb67694beb18e3cb3ff593724157aa1bbd2ea24
Pikatoise <wierwolf@inbox.ru>
2024-05-02 20:11:16 +0500

    Define IRoleService

--- MyFinances.Domain/Interfaces/Services/IRoleService.cs
+++ MyFinances.Domain/Interfaces/Services/IRoleService.cs
@@ -1,9 +1,47 @@
-﻿namespace MyFinances.Domain.Interfaces.Services
+﻿using MyFinances.Domain.DTO.Role;
+using MyFinances.Domain.DTO.UserRole;
+using MyFinances.Domain.Result;
+
+namespace MyFinances.Domain.Interfaces.Services
 {
     /// <summary>
     /// Service for role controle
     /// </summary>
     public interface IRoleService
     {
+        /// <summary>
+        /// Create new role
+        /// </summary>
+        /// <param name="dto">Data for creation</param>
+        /// <returns><c>RoleDto</c>: data of new role</returns>
+        Task<BaseResult<RoleDto>> CreateRoleAsync(CreateRoleDto dto);
+
+        /// <summary>
+        /// Remove role
+        /// </summary>
+        /// <param name="id">Role identificator</param>
+        /// <returns><c>RoleDto</c>: data of deleted role</returns>
+        Task<BaseResult<RoleDto>> DeleteRoleAsync(long id);
+
+        /// <summary>
+        /// Update role
+        /// </summary>
+        /// <param name="dto">Data for updating</param>
+        /// <returns><c>RoleDto</c>: data of updated role</returns>
+        Task<BaseResult<RoleDto>> UpdateRoleAsync(RoleDto dto);
+
+        /// <summary>
+        /// Add role to user
+        /// </summary>
+        /// <param name="dto">Data for adding role to user</param> 
+        /// <returns><c>UserRoleDto</c>: data of user with new role</returns>
+        Task<BaseResult<UserRoleDto>> SetRoleToUserAsync(UserRoleDto dto);
+
+        /// <summary>
+        /// Remove role from user
+        /// </summary>
+        /// <param name="dto">Data for removing role from user</param>
+        /// <returns><c>UserRoleDto</c>: data of user without role</returns>
+        Task<BaseResult<UserRoleDto>> RemoveUserRole(RemoveUserRoleDto dto);
     }
 }
