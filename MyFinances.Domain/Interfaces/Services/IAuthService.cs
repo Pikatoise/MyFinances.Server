6caf8ef1f8a2617c6f3f744006def9c3b70f7b1b
Pikatoise <wierwolf@inbox.ru>
2024-05-02 19:22:26 +0500

    Define IAuthService & add TokenDto

--- MyFinances.Domain/Interfaces/Services/IAuthService.cs
+++ MyFinances.Domain/Interfaces/Services/IAuthService.cs
@@ -1,9 +1,26 @@
-﻿namespace MyFinances.Domain.Interfaces.Services
+﻿using MyFinances.Domain.DTO;
+using MyFinances.Domain.DTO.User;
+using MyFinances.Domain.Result;
+
+namespace MyFinances.Domain.Interfaces.Services
 {
     /// <summary>
     /// Service for user authentication
     /// </summary>
     public interface IAuthService
     {
+        /// <summary>
+        /// Registration
+        /// </summary>
+        /// <param name="dto">Data for registration</param>
+        /// <returns><c>UserDto</c>: data of registred user</returns>
+        Task<BaseResult<UserDto>> Register(RegisterUserDto dto);
+
+        /// <summary>
+        /// Authorization
+        /// </summary>
+        /// <param name="dto">Data for authorization</param>
+        /// <returns><c>TokenDto</c>: tokens for auth</returns>
+        Task<BaseResult<TokenDto>> Login(LoginUserDto dto);
     }
 }
