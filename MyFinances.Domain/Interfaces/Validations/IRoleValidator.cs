496d73f4aa15b7ea863d5f9d1108f366e1f472eb
Pikatoise <wierwolf@inbox.ru>
2024-05-04 20:36:15 +0500

    Define IRoleValidator

---
+++ MyFinances.Domain/Interfaces/Validations/IRoleValidator.cs
@@ -0,0 +1,24 @@
+﻿using MyFinances.Domain.Entity;
+using MyFinances.Domain.Result;
+
+namespace MyFinances.Domain.Interfaces.Validations
+{
+    public interface IRoleValidator: IBaseValidator<Role>
+    {
+        /// <summary>
+        /// Check is userRole exist
+        /// </summary>
+        /// <param name="userRole">Entity to validate</param>
+        /// <returns><c>BaseResult</c>: empty BaseResult, if validation was successful
+        /// <para><c>BaseResult with Error</c>: otherwise</para></returns>
+        BaseResult ValidateOnUserRoleExist(UserRole? userRole);
+
+        /// <summary>
+        /// Check is userRole not exist
+        /// </summary>
+        /// <param name="userRole">Entity to validate</param>
+        /// <returns><c>BaseResult</c>: empty BaseResult, if validation was successful
+        /// <para><c>BaseResult with Error</c>: otherwise</para></returns>
+        BaseResult ValidateOnUserRoleNotExist(UserRole? userRole);
+    }
+}
