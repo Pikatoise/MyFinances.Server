45bafdbc76590e1987c77b5319f069ad8f951f90
Pikatoise <wierwolf@inbox.ru>
2024-05-04 20:24:28 +0500

    Define ITokenValidator

---
+++ MyFinances.Domain/Interfaces/Validations/ITokenValidator.cs
@@ -0,0 +1,17 @@
+﻿using MyFinances.Domain.Entity;
+using MyFinances.Domain.Result;
+
+namespace MyFinances.Domain.Interfaces.Validations
+{
+    public interface ITokenValidator: IBaseValidator<UserToken>
+    {
+        /// <summary>
+        /// Check is user exist, received token similar to origin and token not expired
+        /// </summary>
+        /// <param name="user">Token owner</param>
+        /// <param name="receivedRefreshToken">Refresh token from dto</param>
+        /// <returns><c>BaseResult</c>: empty BaseResult, if validation was successful
+        /// <para><c>BaseResult with Error</c>: otherwise</para></returns>
+        BaseResult ValidateRefreshingToken(User? user, string receivedRefreshToken);
+    }
+}
