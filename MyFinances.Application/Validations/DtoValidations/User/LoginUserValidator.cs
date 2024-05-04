34846c09cffd7c7ab024d965df7f6360710e1cb0
Pikatoise <wierwolf@inbox.ru>
2024-05-04 19:33:37 +0500

    Define LoginUserDto validator

---
+++ MyFinances.Application/Validations/DtoValidations/User/LoginUserValidator.cs
@@ -0,0 +1,20 @@
+﻿using FluentValidation;
+using MyFinances.Application.Resources;
+using MyFinances.Domain.DTO.User;
+
+namespace MyFinances.Application.Validations.DtoValidations.User
+{
+    public class LoginUserValidator: AbstractValidator<LoginUserDto>
+    {
+        public LoginUserValidator()
+        {
+            RuleFor(x => x.Login)
+                .NotEmpty().WithMessage(ErrorMessages.Dto_EmptyField).WithErrorCode("Dto.EmptyField")
+                .MinimumLength(4).WithMessage(ErrorMessages.Dto_TooLowValue).WithErrorCode("Dto.TooLowValue")
+                .MaximumLength(20).WithMessage(ErrorMessages.Dto_TooBigValue).WithErrorCode("Dto.TooBigValue");
+
+            RuleFor(x => x.Password)
+                .NotEmpty().WithMessage(ErrorMessages.Dto_EmptyField).WithErrorCode("Dto.EmptyField");
+        }
+    }
+}
