fdadc6bc8f7eb5d77f8b7e5d531565f9aeccf886
Pikatoise <wierwolf@inbox.ru>
2024-05-04 19:42:20 +0500

    Init RegisterUserDto validator

---
+++ MyFinances.Application/Validations/DtoValidations/User/RegisterUserValidator.cs
@@ -0,0 +1,27 @@
+﻿using FluentValidation;
+using MyFinances.Application.Resources;
+using MyFinances.Domain.DTO.User;
+
+namespace MyFinances.Application.Validations.DtoValidations.User
+{
+    public class RegisterUserValidator: AbstractValidator<RegisterUserDto>
+    {
+        public RegisterUserValidator()
+        {
+            RuleFor(x => x.Login)
+                .NotEmpty().WithMessage(ErrorMessages.Dto_EmptyField).WithErrorCode("Dto.EmptyField")
+                .MinimumLength(4).WithMessage(ErrorMessages.Dto_TooLowValue).WithErrorCode("Dto.TooLowValue")
+                .MaximumLength(20).WithMessage(ErrorMessages.Dto_TooBigValue).WithErrorCode("Dto.TooBigValue");
+
+            RuleFor(x => x.Password)
+                .NotEmpty().WithMessage(ErrorMessages.Dto_EmptyField).WithErrorCode("Dto.EmptyField")
+                .MinimumLength(6).WithMessage(ErrorMessages.Dto_TooLowValue).WithErrorCode("Dto.TooLowValue")
+                .MaximumLength(30).WithMessage(ErrorMessages.Dto_TooBigValue).WithErrorCode("Dto.TooBigValue");
+
+            RuleFor(x => x.PasswordConfirm)
+                .NotEmpty().WithMessage(ErrorMessages.Dto_EmptyField).WithErrorCode("Dto.EmptyField")
+                .MinimumLength(6).WithMessage(ErrorMessages.Dto_TooLowValue).WithErrorCode("Dto.TooLowValue")
+                .MaximumLength(30).WithMessage(ErrorMessages.Dto_TooBigValue).WithErrorCode("Dto.TooBigValue");
+        }
+    }
+}
