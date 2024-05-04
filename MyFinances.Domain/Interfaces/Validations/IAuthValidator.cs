9a19aeb0883f4bb5706368c51fb2659b53e6c0e2
Pikatoise <wierwolf@inbox.ru>
2024-05-04 20:46:56 +0500

    Define IAuthValidator

---
+++ MyFinances.Domain/Interfaces/Validations/IAuthValidator.cs
@@ -0,0 +1,18 @@
+﻿using MyFinances.Domain.Entity;
+using MyFinances.Domain.Result;
+
+namespace MyFinances.Domain.Interfaces.Validations
+{
+    public interface IAuthValidator: IBaseValidator<User>
+    {
+        /// <summary>
+        /// Check is password verified
+        /// </summary>
+        /// <param name="passwordVerifier">Func that verify password and return result</param>
+        /// <param name="userPassword">Right user passoword</param>
+        /// <param name="dtoPassword">Received password from request</param>
+        /// <returns><c>BaseResult</c>: empty BaseResult, if validation was successful
+        /// <para><c>BaseResult with Error</c>: otherwise</para></returns>
+        BaseResult ValidatePasswordVerifying(Func<string, string, bool> passwordVerifier, string userPassword, string dtoPassword);
+    }
+}
