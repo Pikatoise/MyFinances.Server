85ce0a23e7c43aa68e640111603083d0c9bb0243
Pikatoise <wierwolf@inbox.ru>
2024-05-05 19:46:38 +0500

    Refactor Login method in AuthService

--- MyFinances.Application/Services/AuthService.cs
+++ MyFinances.Application/Services/AuthService.cs
@@ -1,12 +1,175 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
+﻿using AutoMapper;
+using Microsoft.EntityFrameworkCore;
+using MyFinances.Domain.DTO;
+using MyFinances.Domain.DTO.User;
+using MyFinances.Domain.Entity;
+using MyFinances.Domain.Enum;
+using MyFinances.Domain.Interfaces.Repositories;
+using MyFinances.Domain.Interfaces.Services;
+using MyFinances.Domain.Interfaces.Validations;
+using MyFinances.Domain.Result;
+using Serilog;
+using System.Security.Claims;
+using System.Security.Cryptography;
 using System.Text;
-using System.Threading.Tasks;
 
 namespace MyFinances.Application.Services
 {
-    internal class AuthService
+    public class AuthService(
+        ILogger logger,
+        IMapper mapper,
+        ITokenService tokenService,
+        IUnitOfWork unitOfWork,
+        IAuthValidator authValidator): IAuthService
     {
+        private readonly ILogger _logger = logger;
+        private readonly IMapper _mapper = mapper;
+        private readonly ITokenService _tokenService = tokenService;
+        private readonly IUnitOfWork _unitOfWork = unitOfWork;
+        private readonly IAuthValidator _authValidator = authValidator;
+
+        public async Task<BaseResult<TokenDto>> Login(LoginUserDto dto)
+        {
+            var user = await _unitOfWork.Users.GetAll()
+                .Include(x => x.Roles)
+                .FirstOrDefaultAsync(u => u.Login.Equals(dto.Login));
+
+            var resultValidationUserOnNull = _authValidator.ValidateOnNull(user);
+
+            if (!resultValidationUserOnNull.IsSuccess)
+                return new BaseResult<TokenDto>()
+                {
+                    Failure = resultValidationUserOnNull.Failure
+                };
+
+            var resultValidationPasswordVerifying = _authValidator.ValidatePasswordVerifying(IsVerifyPassword, user.Password, dto.Password);
+
+            if (!resultValidationPasswordVerifying.IsSuccess)
+                return new BaseResult<TokenDto>()
+                {
+                    Failure = resultValidationPasswordVerifying.Failure
+                };
+
+            var userToken = await _unitOfWork.UserTokens
+                .GetAll()
+                .FirstOrDefaultAsync(x => x.UserId == user.Id);
+
+            var claims = user.Roles.Select(x => new Claim(ClaimTypes.Role, x.Name)).ToList();
+
+            claims.Add(new Claim(ClaimTypes.Name, user.Login));
+
+            var refreshToken = _tokenService.GenerateRefreshToken();
+            var accesToken = _tokenService.GenerateAccessToken(claims);
+
+            if (userToken == null)
+            {
+                userToken = new UserToken()
+                {
+                    UserId = user.Id,
+                    RefreshToken = refreshToken,
+                    RefreshTokenExpiryTime = DateTime.UtcNow.AddDays(7)
+                };
+
+                await _unitOfWork.UserTokens.CreateAsync(userToken);
+            }
+            else
+            {
+                userToken.RefreshToken = refreshToken;
+                userToken.RefreshTokenExpiryTime = DateTime.UtcNow.AddDays(7);
+
+                _unitOfWork.UserTokens.Update(userToken);
+            }
+
+            await _unitOfWork.SaveChangesAsync();
+
+            return new BaseResult<TokenDto>()
+            {
+                Data = new TokenDto()
+                {
+                    AccessToken = accesToken,
+                    RefreshToken = refreshToken,
+                }
+            };
+        }
+
+        public async Task<BaseResult<UserDto>> Register(RegisterUserDto dto)
+        {
+            if (!dto.Password.Equals(dto.PasswordConfirm))
+                return new BaseResult<UserDto>()
+                {
+                    ErrorCode = (int)ErrorCodes.PasswordsAreDifferent,
+                    ErrorMessage = ErrorMessage.PasswordsAreDifferent
+                };
+
+            var user = await _unitOfWork.Users.GetAll().FirstOrDefaultAsync(u => u.Login.Equals(dto.Login));
+
+            if (user != null)
+                return new BaseResult<UserDto>()
+                {
+                    ErrorCode = (int)ErrorCodes.UserAlreadyExist,
+                    ErrorMessage = ErrorMessage.UserAlreadyExist
+                };
+
+            var defaultRole = _unitOfWork.Roles.GetAll().FirstOrDefault(x => x.Name.Equals(nameof(Roles.User)));
+            if (defaultRole == null)
+                return new BaseResult<UserDto>()
+                {
+                    ErrorCode = (int)ErrorCodes.RoleNotFound,
+                    ErrorMessage = ErrorMessage.RoleNotFound
+                };
+
+            var hashedPassword = HashPassword(dto.Password);
+
+            using (var transaction = await _unitOfWork.BeginTransactionAsync())
+            {
+                try
+                {
+                    user = new User()
+                    {
+                        Login = dto.Login,
+                        Password = hashedPassword
+                    };
+
+                    await _unitOfWork.Users.CreateAsync(user);
+
+                    await _unitOfWork.SaveChangeAsync();
+
+                    var userRole = new UserRole()
+                    {
+                        UserId = user.Id,
+                        RoleId = defaultRole.Id
+                    };
+
+                    await _unitOfWork.UserRoles.CreateAsync(userRole);
+
+                    await _unitOfWork.SaveChangeAsync();
+
+                    await transaction.CommitAsync();
+                }
+                catch (Exception)
+                {
+                    await transaction.RollbackAsync();
+                }
+            }
+
+            return new BaseResult<UserDto>()
+            {
+                Data = _mapper.Map<UserDto>(user)
+            };
+        }
+
+        private static string HashPassword(string password)
+        {
+            var bytes = SHA256.HashData(Encoding.UTF8.GetBytes(password));
+
+            return Convert.ToBase64String(bytes);
+        }
+
+        private static bool IsVerifyPassword(string userPasswordHashed, string userPassword)
+        {
+            var hash = HashPassword(userPassword);
+
+            return hash.Equals(userPasswordHashed);
+        }
     }
 }
