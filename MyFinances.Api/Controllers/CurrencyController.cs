4b8d20d54fff020424c7bbe3eff81b11b2960b56
Pikatoise <wierwolf@inbox.ru>
2024-05-07 17:03:55 +0500

    Init & define CurrencyController

---
+++ MyFinances.Api/Controllers/CurrencyController.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using MyFinances.Api.Extensions;
+using MyFinances.Domain.Interfaces.Services;
+
+namespace MyFinances.Api.Controllers
+{
+    /// <summary>
+    /// Currency controller
+    /// </summary>
+    [ApiController]
+    [Route("api/[controller]")]
+    [Authorize]
+    public class CurrencyController(ICurrencyService currencyService): ControllerBase
+    {
+        private readonly ICurrencyService _currencyService = currencyService;
+
+        [HttpGet("{currencyName}")]
+        public async Task<IResult> GetCurrencyValue(string currencyName)
+        {
+            var response = await _currencyService.GetCurrencyValue(currencyName);
+
+            return response.IsSuccess ? Results.Ok(response) : response.ToProblemDetails();
+        }
+    }
+}
