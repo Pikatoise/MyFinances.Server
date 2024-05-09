fa3783bd71a04c97b1363b889e25f0c6d7c3d958
Pikatoise <wierwolf@inbox.ru>
2024-05-09 19:51:12 +0500

    Define RoleMapping

---
+++ MyFinances.Application/Mapping/RoleMapping.cs
@@ -0,0 +1,15 @@
+﻿using AutoMapper;
+using MyFinances.Domain.DTO.Role;
+using MyFinances.Domain.Entity;
+
+namespace MyFinances.Application.Mapping
+{
+    public class RoleMapping: Profile
+    {
+        public RoleMapping()
+        {
+            CreateMap<Role, RoleDto>()
+                .ReverseMap();
+        }
+    }
+}
