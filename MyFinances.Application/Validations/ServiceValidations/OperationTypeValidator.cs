c2635857105b041c27923999f7658a192a2a2c25
Pikatoise <wierwolf@inbox.ru>
2024-05-11 17:57:35 +0500

    Completly define OperationTypeErrors and replace them into OperationTypeValidator

--- MyFinances.Application/Validations/ServiceValidations/OperationTypeValidator.cs
+++ MyFinances.Application/Validations/ServiceValidations/OperationTypeValidator.cs
@@ -1,5 +1,5 @@
-﻿using MyFinances.Application.Resources;
-using MyFinances.Domain.Entity;
+﻿using MyFinances.Domain.Entity;
+using MyFinances.Domain.Errors;
 using MyFinances.Domain.Interfaces.Validations;
 using MyFinances.Domain.Result;
 
@@ -12,7 +12,7 @@ namespace MyFinances.Application.Validations.ServiceValidations
             if (operationType != null)
                 return new BaseResult()
                 {
-                    Failure = Error.Conflict("OperationType.AlreadyExist", ErrorMessages.OperationType_AlreadyExist)
+                    Failure = OperationTypeErrors.OperationTypeAlreadyExist
                 };
 
             return new BaseResult();
@@ -23,13 +23,13 @@ namespace MyFinances.Application.Validations.ServiceValidations
             if (operationType == null)
                 return new BaseResult()
                 {
-                    Failure = Error.NotFound("OperationType.NotFound", ErrorMessages.Currency_NotFound)
+                    Failure = OperationTypeErrors.OperationTypeNotFound
                 };
 
             if (typeAssociation != null)
                 return new BaseResult()
                 {
-                    Failure = Error.Conflict("TypeAssociation.AlreadyExist", ErrorMessages.TypeAssociation_AlreadyExist)
+                    Failure = OperationTypeErrors.TypeAssociationAlreadyExist
                 };
 
             return new BaseResult();
@@ -40,7 +40,7 @@ namespace MyFinances.Application.Validations.ServiceValidations
             if (model == null)
                 return new BaseResult()
                 {
-                    Failure = Error.NotFound("OperationType.NotFound", ErrorMessages.OperationType_NotFound)
+                    Failure = OperationTypeErrors.OperationTypeNotFound
                 };
 
             return new BaseResult();
--- MyFinances.Domain/Errors/OperationTypeErrors.cs
+++ MyFinances.Domain/Errors/OperationTypeErrors.cs
@@ -6,5 +6,9 @@ namespace MyFinances.Domain.Errors
     public static class OperationTypeErrors
     {
         public static readonly Error OperationTypeNotFound = Error.NotFound("OperationType.NotFound", ErrorMessages.OperationType_NotFound);
+
+        public static readonly Error OperationTypeAlreadyExist = Error.Conflict("OperationType.AlreadyExist", ErrorMessages.OperationType_AlreadyExist);
+
+        public static readonly Error TypeAssociationAlreadyExist = Error.Conflict("TypeAssociation.AlreadyExist", ErrorMessages.TypeAssociation_AlreadyExist);
     }
 }
